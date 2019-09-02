6703e755f9f9bb440e0332ae5ae1b642fb1695fe
Bhagyashree Khatri <bhagya.khatri@webonline.com.my>
2019-09-02 18:27:03 +0800

    offer details ui finished

---
+++ ZenjobDemo/ZJOfferDetailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ZJOfferDetailCollectionViewCell.swift
+//  ZenjobDemo
+//
+//  Created by Bhagyashree Haresh Khatri on 02/09/2019.
+//  Copyright © 2019 Bhagyashree Haresh Khatri. All rights reserved.
+//
+
+import UIKit
+
+class ZJOfferDetailCollectionViewCell: UICollectionViewCell {
+    
+}
